28fb8fa193070c24b79d0b2b1bccd17f2bd0d004
Hendra <hendrasyailendra@yahoo.com>
2024-08-31 10:57:55 +0800

    Remove backend call

--- backend/functions/openai_requests.py
+++ backend/functions/openai_requests.py
@@ -8,20 +8,13 @@ from functions.database import get_recent_messages
 # Retrieve Enviornment Variables
 openai.organization = config("OPEN_AI_ORG")
 openai.api_key = config("OPEN_AI_KEY")
-apiKey = config("API_KEY")
-whisperUrl = "https://hackathonopenai-instance.openai.azure.com/openai/deployments/whisper-hackathon-test/audio/translations?api-version=2024-06-01"
-gptUrl = "https://hackathonopenai-instance.openai.azure.com/openai/deployments/Hackathon-GPT4o/chat/completions?api-version=2023-03-15-preview"
 
 # Convert audio to text
 def convert_audio_to_text(audio_file):
   try:
-    #transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    files = {'file': audio_file}
-    headers = {"api-key": apiKey}
-    response = requests.post(whisperUrl, headers=headers, files=files)
-    message = json.loads(response.text)
-    print(message['text'])
-    return message['text']
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    message_text = transcript["text"]
+    return message_text
   except Exception as e:
     print(e)
     return
@@ -31,11 +24,11 @@ def get_chat_response(message_input):
   user_message = {"role": "user", "content": message_input }
   messages.append(user_message)
   try:
-    headers = {"api-key": apiKey}
-    data={"messages": messages}
-    response = requests.post(gptUrl, headers=headers, json=data)
-    print(response.json())
-    message = json.loads(response.text)
-    return message["choices"][0]["message"]["content"]
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages
+    )
+    message_text = response["choices"][0]["message"]["content"]
+    return message_text
   except Exception as e:
     return
